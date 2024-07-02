dccd381f275ac0a5a2ee5d0106eb9436bfc94a54
Mălina Săndulache <166219930+MalinaCatalina@users.noreply.github.com>
2024-07-02 11:34:23 +0300

    Modificare in urma citirii corecte a cerintei

--- Tema - sign in page/index.js
+++ Tema - sign in page/index.js
@@ -1,25 +1,42 @@
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 const form = document.getElementById("formular");
+const email_error = document.getElementById("email-error");
+const password_error = document.getElementById("password-error");
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    if (emailInput.value === "") {
-        alert("Please enter an email!");
-        return;
-    }
+    emailInput.classList.remove('valid', 'invalid');
+    passwordInput.classList.remove('valid', 'invalid');
 
-    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (!emailInput.value.match(emailRegex)) {
-        alert("Please enter a valid email!");
-        return;
+    if (emailInput.value === "" || emailInput.value == null) {
+        email_error.innerHTML = "Please enter a valid email!";
+        email_error.style.color = "red";
+        emailInput.classList.add('invalid');
+    } else {
+        const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (!emailInput.value.match(emailRegex)) {
+            email_error.innerHTML = "Please enter a valid email!";
+            email_error.style.color = "red";
+            emailInput.classList.add('invalid');
+        } else {
+            emailInput.classList.add('valid');
+            email_error.innerHTML = "Email adreess is correct!"
+            email_error.style.color = "green";
+        }
     }
 
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/;
     if (!passwordInput.value.match(passwordRegex)) {
-        alert("Password must contain 1 uppercase letter, 1 lowercase letter, and a digit!");
-        return;
+        password_error.innerHTML = "Password must contain 1 uppercase letter, 1 lowercase letter, and a digit!";
+        passwordInput.classList.add('invalid');
+        password_error.style.color = "red";
+    } else {
+        passwordInput.classList.add('valid');
+        password_error.innerHTML = "Password is correct!"
+        password_error.style.color = "green";
     }
-});
+}
+);
 
